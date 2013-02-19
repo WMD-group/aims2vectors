2b40037c8e8ae0b7ccd70e8643c1a91dd6300ee8
Adam Jackson <a.j.jackson@bath.ac.uk>
2013-02-19 17:59:51 +0000

    Initial commit (code should already be functional)

---
+++ README.md
@@ -0,0 +1,12 @@
+aims2vectors
+============
+Read *FHI-aims*-formatted geometry.in file and return vectors in a, b, c, alpha, beta, gamma form.
+Intended for easy reporting and comparison of periodic unit cells for quantum chemistry.
+
+Requirements
+------------
+Python with Numpy, Unix-like file system.
+
+Disclaimer
+----------
+This script is not affiliated with *FHI-aims*. Feel free to use it and build on it, but do so at your own risk.
---
+++ aims2vectors.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+
+"""Get lattice vector in a, b, c, alpha, beta, gamma format from input geometry"""
+
+import numpy as np
+import sys
+
+# Allow file to be given as input argument
+
+if len(sys.argv) == 1:
+    input_file='geometry.in'
+else:
+    input_file = sys.argv[1]
+
+# Import data columns from FHI-aims input file
+lattice_matrix = np.genfromtxt(input_file,comments='#',usecols=(1,2,3))
+# Truncate to top 3 rows
+lattice_matrix = lattice_matrix[0:3,:]
+
+# Get lattice vector magnitudes (a, b, c) according to Pythagoras' theorem
+(a, b, c) = np.sqrt(np.sum(np.square(lattice_matrix),1))
+
+# 
+alpha = np.arccos(lattice_matrix[0,:].dot(lattice_matrix[1,:]) \
+                    / (np.linalg.norm(lattice_matrix[0,:]) * \
+                           np.linalg.norm(lattice_matrix[1,:]) \
+                      )
+                )
+
+beta = np.arccos(lattice_matrix[1,:].dot(lattice_matrix[2,:]) \
+                    / (np.linalg.norm(lattice_matrix[1,:]) * \
+                           np.linalg.norm(lattice_matrix[2,:]) \
+                      )
+                )
+
+gamma = np.arccos(lattice_matrix[2,:].dot(lattice_matrix[0,:]) \
+                    / (np.linalg.norm(lattice_matrix[2,:]) * \
+                           np.linalg.norm(lattice_matrix[0,:]) \
+                      )
+                )
+
+
+# Convert to degrees
+(alpha, beta, gamma) = np.array([alpha, beta, gamma]).dot(180/np.pi)
+
+print '    a    b      c    alpha  beta   gamma'
+print '{0:6.3} {1:6.3f} {2:6.3f} {3:6.3f} {4:6.3f} {5:6.3f}'.format(\
+          a,    b,    c,    alpha, beta, gamma)
+
